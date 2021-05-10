58a8734451f7031ce0a4127e8d1e91997660ec6d
hameeza <hahmed2305@gmail.com>
2021-05-10 22:44:17 +0500

    Update compilation-IRtoexe.sh

--- LLVM-3.8/compilation-IRtoexe.sh
+++ LLVM-3.8/compilation-IRtoexe.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-LLVM_BIN=/home/hameeza/Downloads/Downloads/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04/bin
-export LD_LIBRARY_PATH=/home/hameeza/Downloads/Downloads/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04/lib:$LD_LIBRARY_PATH
+
+################This converts IR codes to executables################
+LLVM_BIN=/home/abc/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04/bin
+export LD_LIBRARY_PATH=/home/abc/clang+llvm-3.8.0-x86_64-linux-gnu-ubuntu-16.04/lib:$LD_LIBRARY_PATH
 App_name="bzip2d"
 
 for i in output/$App_name/selected-IR/*
 do
-
 fname=`basename $i`
-
 $LLVM_BIN/llc -filetype=obj $i -o output/$App_name/object/$fname.o
 $LLVM_BIN/clang++ output/$App_name/object/$fname.o -o output/$App_name/exe-files/$fname
 echo $fname
 done
-
-
